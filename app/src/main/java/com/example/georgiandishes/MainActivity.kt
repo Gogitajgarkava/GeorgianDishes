0717846a39fe04726c45ecf6e4242bf09cc1aeed
Gogitajgarkava <g.jgarkava@alte.edu.ge>
2025-06-15 21:34:37 +0400

    update

--- app/src/main/java/com/example/georgiandishes/MainActivity.kt
+++ app/src/main/java/com/example/georgiandishes/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.georgiandishes
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        auth = FirebaseAuth.getInstance()
+
+        val mainView = findViewById<View>(R.id.main)
+        mainView.post {
+            if (auth.currentUser != null) {
+
+                goToMainScreen()
+            } else {
+
+                goToSignInScreen()
+            }
+        }
+    }
+    private fun goToMainScreen() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.navigate(R.id.action_signIn_to_main)
+    }
+    private fun goToSignInScreen() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.navigate(R.id.action_main_to_signIn)
     }
 }
