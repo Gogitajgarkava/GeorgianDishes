556d0ca735c60c35ac01e7fcffac7a168f12c402
Gogitajgarkava <g.jgarkava@alte.edu.ge>
2025-06-17 00:30:09 +0400

    update

--- app/src/main/java/com/example/georgiandishes/MainPage/MenuFragment/FragmentMenu.kt
+++ app/src/main/java/com/example/georgiandishes/MainPage/MenuFragment/FragmentMenu.kt
@@ -3,20 +3,29 @@ package com.example.georgiandishes.MainPage.MenuFragment
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.georgiandishes.R
 import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
 
 class FragmentMenu : Fragment(R.layout.fragment_menu) {
 
     private lateinit var logOutButton: Button
     private lateinit var auth: FirebaseAuth
+    private lateinit var LogoPhoto: ImageView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        LogoPhoto = view.findViewById(R.id.imageView2)
+        val url = "https://github.com/Gogitajgarkava/Midd/blob/main/NewLogo.png?raw=true"
+        Picasso.get()
+            .load(url)
+            .into(LogoPhoto)
 
         auth = FirebaseAuth.getInstance()
         logOutButton = view.findViewById(R.id.button4)
@@ -32,3 +41,4 @@ class FragmentMenu : Fragment(R.layout.fragment_menu) {
         findNavController().navigate(R.id.action_fragmentProfile_to_fragmentSingIn)
     }
 }
+
